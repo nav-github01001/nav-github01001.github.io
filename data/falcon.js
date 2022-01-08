f4b3c7f00fd13b71be8a70e21ab1719663169e14
nav-github01001 <krishnanav309@gmail.com>
2022-01-08 16:57:05 +0800

    LoLity

--- data/falcon.js
+++ data/falcon.js
@@ -1,5 +1,15 @@
+document.cookie= "projectClicked = False";
 function projectClick(){
-    document.getElementById("project-view").innerHTML = "lol"
+    console.log("ede9idihdehduehieiehide")
+    if (document.cookie == "projectClicked = False"){
+        document.getElementById("project-view").innerHTML = "lol";
+        document.cookie= "projectClicked=True";
+    }
+    else if(document.cookie == "projectClicked = True"){
+        document.getElementById("project-view").innerHTML = "lolity";
+        document.cookie= "projectClicked=True";
+    }
+
 }
 
 
