4aba86ddbbfbcd04bf490a072c94f84cb9f9a016
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-02-02 20:05:39 +0800

    Added another link

--- data/falcon.js
+++ data/falcon.js
@@ -3,7 +3,7 @@ function projectClick(){
     console.log("ede9idihdehduehieiehide")
     if (document.cookie == "projectClicked = False"){
         document.getElementById("project-view").innerHTML = "lol";
-        document.cookie= "projectClicked=True";
+        document.cookie= "projectClicked = True";
     }
     else if(document.cookie == "projectClicked = True"){
         document.getElementById("project-view").innerHTML = "lolity";
